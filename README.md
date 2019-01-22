3f227347b4b240e6449fbc5269329b7d024907ff
ronoysarkar <ronoysarkar.mail@gmail.com>
2019-01-21 21:28:28 -0500

    Updated data sets

--- README.md
+++ README.md
@@ -2,5 +2,3 @@
 Using machine learning and allele information to predict success rate of a prescription drug on an individual.
 
 https://ronoys.github.io/predicting-prescription-drug-reaction-from-genetic-variations/
-
-test commit
--- drug_data_sets/filtering.py
+++ drug_data_sets/filtering.py
@@ -13,6 +13,29 @@ while x < 10:
 import os
 
 
-with open(os.path.join(os.getcwd(), 'synthroid_data.txt'), 'r') as text_file:
+with open(os.path.join(os.getcwd(), 'sample_data.txt'), 'r') as text_file:
+    count = 1
     for line in text_file:
-        print line
+        x = str(line)
+        #print x[:2]
+        if x[0:2] == 'rs':
+            print x
+        
+        #try:
+        #    c = float(line)
+        #    if c < 1:
+        #        print c
+
+        #except:
+        #    print "-"
+        '''if count == 1 or count == 2 or count == 6:
+            print line
+
+        if count == 13:
+            count = 1
+        count = count + 1
+        '''
+        #print str (line)
+
+#print os.path.join(os.getcwd(), 'hello.txt') - prints C:\Users\ronoy\AppData\Local\atom\Atom Files\hello.txt
+#print os.getcwd()
